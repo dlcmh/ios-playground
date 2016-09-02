929fc6fdcda5c03127cefc6605d62deb59b6a6ac
David Chin <dlcmhd@me.com>
2016-09-02 22:02:46 +0800

    baseline

---
+++ Names to Faces/Names to Faces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Names to Faces
+//
+//  Created by David Chin on 23/09/2016.
+//  Copyright © 2016 Dakerr Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# [Project 10: Names to Faces](https://www.hackingwithswift.com/read/10/overview)
+
+Re-work Project 7 to learn how to run complex tasks in the background with GCD.UICollectionView, UIImagePickerController, UUID, classes
+
+Get started with UICollectionView and the photo library.
+
+[Click for full size](https://raw.githubusercontent.com/dlcmh/ios-playground/hws-09-grand-central-dispatch/1.png)
+![...](1.png "...")
