fd4fa7b1d98741a2f96adabc7e2a3c4940af4820
David Chin <dlcmhd@me.com>
2016-09-04 15:12:44 +0800

    Core Animation animations, touches, transforms

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO:
+
+- http://jacobsalmela.com/make-animated-gifs-in-os-x-with-a-right-click/
+- https://github.com/mortenjust/droptogif
+- http://osxdaily.com/2016/04/08/make-animated-gif-from-movie-mac-os-x-drop-to-gif/
--- iOS Playground/ViewController.swift
+++ iOS Playground/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var scaleFactor: CGFloat = 2
+    var angle: Double = 180
+    var boxView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let frameRect = CGRectMake(20, 20, 45, 45)
+        
+        boxView = UIView(frame: frameRect)
+        boxView?.backgroundColor = UIColor.blueColor()
+        view.addSubview(boxView!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.locationInView(view)
+                        
+            UIView.animateWithDuration(
+                0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut,
+                animations: {
+                    let scaleTrans = CGAffineTransformMakeScale(self.scaleFactor, self.scaleFactor)
+                    let rotateTrans = CGAffineTransformMakeRotation(CGFloat(self.angle * M_PI / 180))
+                    self.boxView!.transform = CGAffineTransformConcat(scaleTrans, rotateTrans)
+                    self.angle = (self.angle == 180 ? 360 : 180)
+                    self.scaleFactor = (self.scaleFactor == 2 ? 1 : 2)
+                    self.boxView!.center = location
+                },
+                completion: nil)
+        }
+    }
 
 }
 
