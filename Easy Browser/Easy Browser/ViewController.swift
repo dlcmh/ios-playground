aa10b0340607326b9f5b056fcd26fef2e57afa28
David Chin <dlcmhd@me.com>
2016-09-11 10:29:23 +0800

    Code

--- Easy Browser/Easy Browser/ViewController.swift
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -7,12 +7,96 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView! // the ! at the end of WKWebView is needed because the property starts out as `nil` before being set later on -> otherwise `class ViewController` line will have a 'Class ViewController has no initializers' error
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self // Dear `WKWebView`, when any web page navigation happens, please tell me. -> add `, WKNavigationDelegate` to `class ViewController` line, otherwise this line will show a 'Cannot assign value of type ViewController to type WKNavigationDelegate?'
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "https://" + websites[0])
+        webView.loadRequest(NSURLRequest(URL: url!))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: #selector(openTapped))
+        
+        // KVO setup for progressView
+        // self: who the observer is (we're the observer, so we use self)
+        // forKeyPath (rather than forProperty) - allows for chaining of property names (eg averaging all elements in an array)
+        // context: if you provide a unique value, that same context value gets sent back to you when you get your notification that the value has changed. This allows you to check the context to make sure it was your observer that was called.
+        // Once you have registered as an observer using KVO, you must implement a method called observeValueForKeyPath()
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
+        
+        // Need to wrap progressView inside a bar button item before using it inside toolbarItems
+        progressView = UIProgressView(progressViewStyle: .Default)
+        progressView.sizeToFit() // tells the progress view set its layout size so that it fits its contents fully
+        let progressButton = UIBarButtonItem(customView: progressView) // wrapping happens here
+
+        let spacer = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .Refresh, target: webView, action: #selector(webView.reload))
+
+        toolbarItems = [progressButton, spacer, refresh] // you'll see the refresh button neatly aligned to the right – that's the effect of the flexible space automatically taking up as much room as it can on the left
+        navigationController?.toolbarHidden = false
+    }
+    
+    // Once you have registered as an observer using KVO, you must implement a method called observeValueForKeyPath()
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath == "estimatedProgress" {
+            let progress = Float(webView.estimatedProgress)
+            progressView.progress = progress
+            print(progress)
+            if progress == 1.0 {
+                progressView.hidden = true
+            } else {
+                progressView.hidden = false
+            }
+        }
+    }
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .ActionSheet)
+//        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+//        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .Default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .Default, handler: openPage))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        // ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem // add this if coding for iPad / Universal devices
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        let url = NSURL(string: "https://" + action.title!)
+        webView.loadRequest(NSURLRequest(URL: url!))
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.URL
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.rangeOfString(website) != nil {
+                    decisionHandler(.Allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.Cancel)
     }
 
     override func didReceiveMemoryWarning() {
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # [Tutorial 4: Easy Browser - loadView(), WKWebView, NSURLRequest, UIToolbar, UIProgressView, key-value observing](https://www.hackingwithswift.com/read/4/overview)
 
-Embed Web Kit and learn about delegation, KVO, classes and UIToolbar.
+Embed Web Kit (and use code instead of Interface Builder to perform layout and add views) and learn about delegation, KVO, classes and UIToolbar.
 
 loadView()
 
-WKWebView
+WKWebView, decidePolicyForNavigationAction (allow/disallow following of links via whitelisting)
 
 delegation
 
@@ -12,7 +12,7 @@ classes and structs
 
 NSURLRequest
 
-UIToolbar: a toolbar component that holds `UIBarButtonItem`s
+UIToolbar: a toolbar component that holds `UIBarButtonItem`s -> refer to [https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/UIKitUICatalog/UIToolbar.html#//apple_ref/doc/uid/TP40012857-UIToolbar-SW1](https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/UIKitUICatalog/UIToolbar.html#//apple_ref/doc/uid/TP40012857-UIToolbar-SW1)
 
 UIProgressView
 
@@ -21,3 +21,26 @@ key-value observing
 UIAlertController
 
 NSURL
+
+
+## Steps
+
+Embed View Controller in Navigation Controller
+
+`import WebKit` in 'ViewController.swift'
+
+Instead of merely overriding `viewDidLoad()`, add a `loadView()` function to override the actual loading of the view.
+
+`UIAlertController` to allow users to select from a list of websites to open - will be triggered from a `UIBarButtonItem` that displays a title instead of a system icon (eg as displayed by the `UIBarButtonItem style: .Action` button)
+
+
+Setting the web page title in the Navigation Controller bar (type webvididfina for function creation autocompletion):
+
+```swift
+func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+    title = webView.title
+}
+```
+
+[Click for full size](https://raw.githubusercontent.com/dlcmh/ios-playground/hws-04-easy-browser/1.png)
+![UINavigationBar & navigationItem, UIToolbar & toolbarItems, UIProgressView)](1.png "UINavigationBar & navigationItem, UIToolbar & toolbarItems, UIProgressView)")
