391694e6f9e820308f04805cf1d439203ab97417
David Chin <dlcmhd@me.com>
2016-09-11 16:08:30 +0800

    Reading from disk: contentsOfFile

--- WordScramble/WordScramble/MasterViewController.swift
+++ WordScramble/WordScramble/MasterViewController.swift
@@ -6,15 +6,36 @@
 //  Copyright © 2016 Dakerr Consulting. All rights reserved.
 //
 
+import GameKit
 import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    var objects = [String]()
+    var allWords = [String]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Load the start.txt file
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+                allWords = startWords.componentsSeparatedByString("\n")
+                
+            } else {
+                allWords = ["silkworm"]
+            }
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+        title = allWords[0]
+        objects.removeAll(keepCapacity: true)
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,8 +57,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
