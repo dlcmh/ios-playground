0f051e42e422458e6ba17455398fe71a2d8a89ce
David Chin <dlcmhd@me.com>
2016-10-02 22:54:50 +0800

    stuff

--- Learn NSUrlSession/Learn NSUrlSession/ViewController.swift
+++ Learn NSUrlSession/Learn NSUrlSession/ViewController.swift
@@ -10,9 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let url_to_request:String = "http://www.kaleidosblog.com/tutorial/nsurlsession_tutorial.php"
+
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    var dataTry: String = "" {
+        didSet {
+            dataLabel.text = dataTry
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+//        data_request()
+        use_me()
+    }
+    
+    func use_me() {
+        print("starting...")
+        
+        let url:URL = URL(string: url_to_request)!
+        print("\(url)")
+        
+        let session = URLSession.shared
+        print("\(session)")
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        let paramString = "data=Hello"
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        print("\(request)")
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
+                print("error")
+                return
+            }
+            
+            DispatchQueue.main.async {
+//                self.friendLabel.text = "You are following \(friendCount) accounts"
+                self.dataTry = String(data: data!, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue))!
+            }
+        })
+        
+        task.resume()
+        
+        print("finished!")
+    }
+    
+    func data_request() {
+        let url:URL = URL(string: url_to_request)!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+        let paramString = "data=Hello"
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
+                print("error")
+                return
+            }
+            
+            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print(dataString)
+            
+        })
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
