0487a7ad737f61935001af03d1b8fef05941af14
David Chin <dlcmhd@me.com>
2016-10-03 19:40:24 +0800

    stuff

--- Learn NSUrlSession/Learn NSUrlSession/ViewController.swift
+++ Learn NSUrlSession/Learn NSUrlSession/ViewController.swift
@@ -10,67 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var dataLabel1: UILabel!
-    @IBOutlet weak var dataLabel2: UILabel!
-    
-    var data1: String = "" {
-        didSet {
-            dataLabel1.text = data1
-        }
-    }
-
-    var data2: String = "" {
-        didSet {
-            dataLabel2.text = data2
-        }
-    }
+    @IBOutlet weak var jwtLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        postMeWithParams(url: "http://www.kaleidosblog.com/tutorial/nsurlsession_tutorial.php")
         postMeWithJSONData(url: "https://brickler-s.herokuapp.com/user/signin")
     }
     
-    func postMeWithParams(url: String) {
-        data1 = "Loading..."
-        
-        let endpoint = URL(string: url)!
-        let request = NSMutableURLRequest(url: endpoint)
-        request.httpMethod = "POST"
-        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
-        let paramString = "data=Hello"
-        request.httpBody = paramString.data(using: String.Encoding.utf8)
-        
-        let session = URLSession.shared
-        let task = session.dataTask(with: request as URLRequest, completionHandler: {
-            (data, response, error) in
-            
-            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
-                print("error")
-                return
-            }
-            
-            DispatchQueue.main.async {
-                self.data1 = String(data: data!, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue))!
-            }
-        })
-        
-        task.resume()
-    }
-
     // http://icodealot.com/sending-http-post-requests-to-ords-from-swift/
     func postMeWithJSONData(url: String) {
         do {
             let loginDict = ["auth": ["email": "user1@a.com", "password": "12345678"]]
             
             let jsonData = try JSONSerialization.data(withJSONObject: loginDict, options: [])
-            let decoded = try JSONSerialization.jsonObject(with: jsonData, options: [])
-            if let dictFromJSON = decoded as? [String: [String: String]] {
-                print(dictFromJSON)
-            }
             
-            data2 = "Loading..."
+            jwtLabel.text = "Loading..."
             
             let endpoint = URL(string: url)!
             
@@ -83,7 +38,6 @@ class ViewController: UIViewController {
             let session = URLSession.shared
             let task = session.dataTask(with: request as URLRequest, completionHandler: {
                 (data, response, error) in
-//                print("Got data \(data) and response \(response) with error \(error)")
                 do {
                     // https://developer.apple.com/swift/blog/?id=37
                     let jsonWithObjectRoot = try JSONSerialization.jsonObject(with: data!, options: [])
@@ -91,7 +45,7 @@ class ViewController: UIViewController {
                     if let dictionary = jsonWithObjectRoot as? [String: Any] {
                         if let jwt = dictionary["jwt"] as? String {
                             DispatchQueue.main.async {
-                                self.data2 = jwt
+                                self.jwtLabel.text = jwt
                             }
                         }
                     }
@@ -108,9 +62,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
